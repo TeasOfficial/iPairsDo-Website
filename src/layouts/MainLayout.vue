388b6b1c3577a830ee2e733c74b72b8560058af9
OriginalSnow <70016672+IM2eR0@users.noreply.github.com>
2024-11-20 18:43:26 +0800

    初始主题、布局

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="lhr lpR lfr">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer reveal>
+      <center>
+        Powered by <a href="#">NekoGan</a>
+        <br>
+        <div class="alert">发现违规内容请及时举报！我们将在第一时间删除相关内容！</div>
+      </center>
+    </q-footer>
+  </q-layout>
+
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+</script>
+
+<style lang="scss" scoped>
+.q-footer {
+  background: #3922aa;
+  color: white;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+  font-style: oblique 5deg;
+}
+
+.alert {
+  font-size: 14px;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-page class="flex flex-center m-bg">
+    <center class="mainbox">
+      <h3>还没想好</h3>
+      <hr>
+      <div>不仅没想好，也没做完！</div>
+      <div>但是呢！</div>
+      <div>点击下方按钮捞一个 <span>
+          “漂流瓶”
+          <q-tooltip transition-show="flip-right" transition-hide="flip-left"
+            style="font-size: 16px; background-color: white; color: black;" anchor="center middle" self="center middle">
+            读作：提示框
+          </q-tooltip>
+        </span> 试试吧~</div>
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn push rounded color="primary" label="捞一个！" @click="showNotif" />
+        <q-btn push rounded color="primary" label="丢一个！" @click="showNotif" />
+      </div>
+    </center>
+  </q-page>
+</template>
+<style lang="scss" scoped>
+.mainbox {
+  padding: 20px;
+  background-color: white;
+  color: black;
+  border-radius: 32px;
+  max-width: 75vw;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  padding: 0;
+  margin: 0;
+}
+</style>
+<script>
+import { Notify } from 'quasar'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "IndexPage",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    showNotif() {
+      Notify.create({
+        message: '还没做后端呢 >.<',
+        html: true,
+        position: 'top-right',
+        color: "blue-grey"
+      })
+    }
+  }
+});
+</script>
